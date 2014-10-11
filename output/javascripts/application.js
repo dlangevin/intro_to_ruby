414edfaf7f23fdafc90d1999e4b3656499720e84
Dan Langevin <dan.langevin@gmail.com>
2014-10-11 10:37:27 -0400

    - Adds updating of the headers - Refactor application.js to support updating of headers

--- output/javascripts/application.js
+++ output/javascripts/application.js
@@ -11,23 +11,32 @@ IntroToRuby.prototype = {
     this.element.find('select').on('change', function(event){
       // Gets the value of the select
       filter = $(this).val();
-
-      if(filter == 'all') {
-        _this.element.find('tbody tr').show();
-        return;
-      }
-
-      // Show the appropriate rows
-      _this.element
-        .find("tbody tr[data-pet-type='" + filter + "']")
-        .show();
-
-      // Hide the other rows
-      _this.element
-        .find("tbody tr")
-        .not("[data-pet-type='" + filter + "']")
-        .hide();
+      _this.showAppropriateRows(filter);
+      _this.updateHeader(filter);
     });
+  },
+  showAppropriateRows: function(filter){
+    if(filter == 'all') {
+      this.element.find('tbody tr').show();
+      return;
+    }
+    this.visibleRows(filter).show();
+    this.hiddenRows(filter).hide();
+  },
+  visibleRows: function(filter){
+    return this.element
+      .find("tbody tr[data-pet-type='" + filter + "']");
+  },
+  hiddenRows: function(filter){
+    return this.element
+      .find("tbody tr")
+      .not("[data-pet-type='" + filter + "']");
+  },
+  updateHeader: function(filter){
+    this.element.find('.pet-count').text(
+      this.visibleRows(filter).length
+    );
+    this.element.find('.pet-type').text(filter);
   }
 };
 
