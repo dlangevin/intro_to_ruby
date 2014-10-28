f1db20c1f4a9eb1ce66868e862b9ced6e6f723a1
Dan Langevin <dan.langevin@gmail.com>
2014-10-27 20:28:11 -0400

    Backend filtering

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,6 +4,11 @@ class PetsController < ApplicationController
 
   def index
     @pets = Pet.all
+
+    if params[:pet_type]
+      @pets = @pets.where(pet_type: params[:pet_type])
+    end
+
     @pet_types = @pets.map(&:pet_type).uniq
 
     respond_with(@pets)
